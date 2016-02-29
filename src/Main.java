c471cacf3bb3858aff3ea1c93916ad8e6a4a0052
Jonathan Brem <jon.brem@t-online.de>
2016-02-29 09:16:25 +0100

    dummy implementation of joining & filtering
    needs discussion!!

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import de.ur.ahci.model.Frame;
 import de.ur.ahci.offline_dictionary.CaseInsensitiveDictionary;
 import de.ur.ahci.offline_dictionary.OfflineDictionaryBoost;
 import de.ur.ahci.string_similarity.NeedlemanWunschSimilarity;
+import de.ur.ahci.string_similarity.StringSimilarityCalculator;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -25,16 +26,17 @@ public class Main {
      * (that is, the version we "distribute" enters here).
      */
     public static void main(String[] args) {
-        List<Frame> frameList = getFrameListFromFile("output_1.json");
+        List<Frame> frameList = getFrameListFromFile("output_9.json");
+        StringSimilarityCalculator similarityCalculator = new NeedlemanWunschSimilarity();
 
-        WordValidator validator = new WordValidator(frameList, new NeedlemanWunschSimilarity());
+        WordValidator validator = new WordValidator(frameList, similarityCalculator);
         validator.run();
         List<StringProbability> probabilities = validator.getStringProbabilities();
 
         applyNGramProbabilityBoost(probabilities);
         applyOfflineDictionaryBoost(probabilities);
 
-        filterAndJoinWords(probabilities);
+//        filterAndJoinWords(probabilities, similarityCalculator);
 
         findSimilarAndSuperCategoriesOnline(probabilities);
 
@@ -52,8 +54,81 @@ public class Main {
         });
     }
 
-    private static void filterAndJoinWords(List<StringProbability> probabilities) {
+    private static void filterAndJoinWords(List<StringProbability> probabilities, StringSimilarityCalculator similarityCalculator) {
+        joinWords(probabilities, similarityCalculator);
+        filterWords(probabilities);
+    }
 
+    private static void filterWords(List<StringProbability> probabilities) {
+
+    }
+
+    private static void joinWords(List<StringProbability> probabilities, StringSimilarityCalculator similarityCalculator) {
+        int index = 0;
+        while(index < probabilities.size()) {
+
+            // set up loop
+            boolean removeCurrent = false;
+            int joinCurrent = -1;
+            double strongestMatchProbability = 0;
+            double strongestMatchStrength = Double.MAX_VALUE;
+
+            List<Integer> joinToCurrentIndices = new ArrayList<>(); // these will be removed & their likelihoods will be added to the current word
+            StringProbability current = probabilities.get(index);
+            String currentWord = current.string.toLowerCase();
+
+            // decide if other words will be joined to this word or vice versa
+            for(int i = index + 1; i < probabilities.size(); i++) {
+                StringProbability other = probabilities.get(i);
+                String otherWord = other.string.toLowerCase();
+
+                if(isPartOf(currentWord, otherWord)) {
+                    if(other.probability >= 1.2 * current.probability && other.probability > strongestMatchProbability) {
+                        strongestMatchStrength = 0;
+                        strongestMatchProbability = other.probability;
+                        removeCurrent = true;
+                        joinCurrent = i;
+                    }
+                } else if (isPartOf(otherWord, currentWord)) {
+                    if(current.probability >= 1.2 * other.probability) {
+                        joinToCurrentIndices.add(i);
+                    }
+                } else {
+                    double similarity = getSimilarity(currentWord, otherWord, similarityCalculator);
+                    if(similarity < 0.25) {
+                        if(other.probability >= 1.2 * current.probability && similarity <= strongestMatchStrength && other.probability > strongestMatchProbability) {
+                            strongestMatchStrength = similarity;
+                            strongestMatchProbability = other.probability;
+                            removeCurrent = true;
+                            joinCurrent = i;
+                        } else if (current.probability >= 1.2 * other.probability) {
+                            joinToCurrentIndices.add(i);
+                        }
+                    }
+                }
+            }
+
+            // enact possible joins
+            if(removeCurrent) {
+//                System.out.println("Removing " + currentWord + ", joining with " + probabilities.get(joinCurrent).string);
+//                probabilities.get(joinCurrent).probability += current.probability;
+                probabilities.remove(index);
+            } else {
+                for(int i = 0; i < joinToCurrentIndices.size(); i++) {
+//                    current.probability += probabilities.get(i).probability;
+                    probabilities.remove(i);
+                }
+                index++;
+            }
+        }
+    }
+
+    private static double getSimilarity(String current, String other, StringSimilarityCalculator similarityCalculator) {
+        return similarityCalculator.getSimilarity(current, other);
+    }
+
+    private static boolean isPartOf(String w1, String w2) {
+        return w2.contains(w1);
     }
 
     private static void applyOfflineDictionaryBoost(List<StringProbability> probabilities) {
@@ -82,10 +157,10 @@ public class Main {
 
 
     public static void addFramesToList(JSONObject frameWrapper, List<Frame> frameList) {
-        for(String key : frameWrapper.keySet()) { // only one key, e.g. "frame5"
+        for(String key : frameWrapper.keySet()) { // only one key, e.g. "frame_5"
             JSONObject frameJson = frameWrapper.getJSONObject(key);
             Frame frame = new Frame();
-            frame.setNumber(Integer.parseInt(key.substring(5)));
+            frame.setNumber(Integer.parseInt(key.substring(6)));
 
             for(String recognitionKey : frameJson.keySet()) {
                 JSONObject recognition = frameJson.getJSONObject(recognitionKey);
--- src/de/ur/ahci/string_similarity/StringSimilarityCalculator.java
+++ src/de/ur/ahci/string_similarity/StringSimilarityCalculator.java
@@ -1,7 +1,7 @@
 package de.ur.ahci.string_similarity;
 
 /**
- * Calculates the reverse of any kind of String distance (i.e., how similar two Strings are, 1 being a perfect match).
+ * Calculates String distances (i.e., how similar two Strings are, 0 being a perfect match).
  */
 public interface StringSimilarityCalculator {
 
